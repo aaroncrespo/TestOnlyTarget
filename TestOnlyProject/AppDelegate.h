acdec3cbc59536347be71fb0b366c7904c25845f
Crespo, Aaron <aaroncrespo@gmail.com>
2015-02-06 09:56:52 -0500

    Initial Commit

---
+++ TestOnlyProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestOnlyProject
+//
+//  Created by Crespo, Aaron on 2/6/15.
+//  Copyright (c) 2015 Crespo, Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestOnlyProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestOnlyProject
+//
+//  Created by Crespo, Aaron on 2/6/15.
+//  Copyright (c) 2015 Crespo, Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
